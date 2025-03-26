28f40d2ad39502fd6f0ace60c713ffa29be07486
Sofía Millán <sofiaisabellamillan16@gmail.com>
2025-03-26 01:06:51 -0500

    Unit tests for TournamentJpaAdapter

--- src/test/java/com/assessment/tournament/infrastructure/CategoryJpaAdapterTest.java
+++ src/test/java/com/assessment/tournament/infrastructure/CategoryJpaAdapterTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class CategoryJpaAdapterTest {
+class CategoryJpaAdapterTest {
     @Mock
     private CategoryRepository categoryRepository;
     @Mock
